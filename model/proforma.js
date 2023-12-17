a9701655cab56d6afc05b9b31a8aadc2810befac
sergio3099 <rodriguezlopezsergio1@gmail.com>
2023-12-17 03:03:26 -0500

    proforma model

--- model/proforma.js
+++ model/proforma.js
@@ -1,10 +1,30 @@
 const {Schema, model} = require('mongoose')
 
 const  ProformaSchema = Schema({
-    idCliente:{
-        type: Schema.Types.ObjectId,
-        ref:'Cliente',
-        required: [true, 'El cliente es obligatorio en la proforma']
+    nombre: {
+        type: String,
+        require: [true,  'el nombre del cliente es obligatorio'],
+        unique: true
+    },
+    apellido: {
+        type: String,
+        required: [true, 'El apellido del cliente es obligatorio']
+    },
+    estado: {
+        type: Boolean,
+        default: true,
+        required: true
+    },
+    direccion: {
+        type: String,
+        required: [true, 'La dirección del cliente es obligatorio']
+    },
+    referencia: {
+        type: String,
+        require: [true, 'la referencia del cliente es obligatoria']
+    },
+    celular:{
+        type: Number,
     },
     idProducto: {
         type: Schema.Types.ObjectId,
@@ -25,11 +45,6 @@ const  ProformaSchema = Schema({
         type: Schema.Types.ObjectId,
         ref: 'Medida',
         required: [true, 'Las medidas son obligatorias en la proforma']
-    },
-    estado: {
-        type: Boolean,
-        default: true,
-        required: true
     }
 })
 
